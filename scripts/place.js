586342d7196783d8aafaa9564b501abdd44a5d0d
Richard-Kouadio <Richardkouamek@gmail.com>
2026-07-18 13:56:20 +0000

    Finished Country Code

--- scripts/place.js
+++ scripts/place.js
@@ -1,39 +1,24 @@
-```javascript
 const temperature = 28;
-
 const windSpeed = 12;
 
 
-
 function calculateWindChill(temp, speed) {
-
     return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
-
 }
-
 
 
 let windChill = "N/A";
 
 
-
 if (temperature <= 10 && windSpeed > 4.8) {
-
-    windChill = `${ calculateWindChill(temperature, windSpeed) } °C`;
-
+    windChill = `${calculateWindChill(temperature, windSpeed)} °C`;
 }
-
 
 
 document.querySelector("#windChill").textContent = windChill;
 
 
-
-document.querySelector("#year").textContent =
-    new Date().getFullYear();
+document.querySelector("#year").textContent = new Date().getFullYear();
 
 
-
-document.querySelector("#lastModified").textContent =
-    document.lastModified;
-```
+document.querySelector("#lastModified").textContent = document.lastModified;
